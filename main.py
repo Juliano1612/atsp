37050be18c350fc06d95ba80cf402fff9e4039cd
Juliano <jcezarchagas@gmail.com>
2019-11-15 18:37:01 -0300

    initial clarke-wright and 2opt working

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ GRAPH_MATRIX = None
 SHORTEST_PATHS = None
 ECONOMIES_DICTS = None
 VISITED = []
+SOLUTIONS = []
 
 PROBLEMS = open('problems.txt', 'w')
 
@@ -45,7 +46,7 @@ def parseFile(path):
 def calculateMinPathsOneToAll():
     global GRAPH_MATRIX, SHORTEST_PATHS
     SHORTEST_PATHS = ssp.csgraph.bellman_ford(GRAPH_MATRIX, directed=True, return_predecessors=False)
-    print(SHORTEST_PATHS)
+    # print(SHORTEST_PATHS)
 
 
 def calculateEconomies():
@@ -60,9 +61,9 @@ def calculateEconomies():
     for key in ECONOMIES_DICTS.keys():
         ECONOMIES_DICTS[key] = dict(sorted(
             ECONOMIES_DICTS[key].items(), key=lambda x : x[1], reverse=True))
-    print("########################################")
-    print(ECONOMIES_DICTS)
-    print("########################################")
+    # print("########################################")
+    # print(ECONOMIES_DICTS)
+    # print("########################################")
 
 
 def removeOriginsDestinies(origin, destiny):
@@ -74,17 +75,52 @@ def removeOriginsDestinies(origin, destiny):
             ECONOMIES_DICTS[key].pop(destiny)
 
 
+def calculatePathSize(path):
+    global GRAPH_MATRIX
+    pathSize = 0
+    for edge in path:
+        # print(edge,GRAPH_MATRIX[edge[0]][edge[1]])
+        if GRAPH_MATRIX[edge[0]][edge[1]] != 0:
+            pathSize += GRAPH_MATRIX[edge[0]][edge[1]]
+        else: 
+            pathSize = 9999
+            break
+    return pathSize
+
+
 def clarkeWright():
-    global ECONOMIES_DICTS, VISITED, DIMENSION
+    global ECONOMIES_DICTS, VISITED, DIMENSION, GRAPH_MATRIX
     VISITED = []
     curr_v0 = 0 # start from zero
     while len(VISITED) < DIMENSION:
-        edge = None
+        edge = None 
         curr_v1 = list(ECONOMIES_DICTS[curr_v0].keys())[0]
-        edge = (curr_v0, curr_v1, ECONOMIES_DICTS[curr_v0][curr_v1])
-        removeOriginsDestinies(curr_v0, curr_v1)
-        VISITED.append(edge)
-        curr_v0 = curr_v1
+        if GRAPH_MATRIX[curr_v0][curr_v1] != 0:
+            # edge = (curr_v0, curr_v1, ECONOMIES_DICTS[curr_v0][curr_v1])
+            edge = (curr_v0, curr_v1)
+            removeOriginsDestinies(curr_v0, curr_v1)
+            VISITED.append(edge)
+            curr_v0 = curr_v1
+        else:
+            ECONOMIES_DICTS[curr_v0].pop(curr_v1)
+    SOLUTIONS.append((VISITED, calculatePathSize(VISITED)))
+
+
+def _2opt():
+    global SOLUTIONS, GRAPH_MATRIX
+    reference_solution = SOLUTIONS[0][0]
+    n = 0
+    while n < len(reference_solution):
+        for i in range(n+2, len(reference_solution)-1 if n == 0 else (len(reference_solution))):
+            edge_one = reference_solution[n]
+            edge_two = reference_solution[i]
+            new_solution = reference_solution.copy()
+            for j in range(n+1, i):
+                new_solution[j] = (new_solution[j][1], new_solution[j][0])
+            new_solution[n] = (reference_solution[n][0], reference_solution[i][0])
+            new_solution[i] = (reference_solution[n][1], reference_solution[i][1])
+            SOLUTIONS.append((new_solution, calculatePathSize(new_solution)))
+        n += 1
 
 
 def main():
@@ -99,9 +135,12 @@ def main():
                         parseFile('grafos/atsp/br17.atsp')
                         calculateMinPathsOneToAll()
                         calculateEconomies()
-                        print(ECONOMIES_DICTS)
                         clarkeWright()
-                        print(VISITED)
+                        # print(SOLUTIONS[0])
+                        # print('\n')
+                        _2opt()
+                        # ordenar as soluções em ordem crescente de custo e pegar a menor
+                        print(sorted(SOLUTIONS, key=lambda x : x[1])[0])
                     except Exception as e:
                         print(e)
                         PROBLEMS.writelines(file + '\n')
@@ -111,4 +150,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
